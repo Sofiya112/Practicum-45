1b50b43e8e37b981d50cccb7c0deb7b5ce800f82
Sofiya112 <sofiya032005@gmail.com>
2025-05-30 12:49:04 +0300

    Add files via upload

---
+++ Задача 41/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_41
+{
+    class Point <T>
+    {
+        private T x { get; }
+        private T y { get; }
+
+        public Point(T x, T y) //Конструктор с параметрами
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void Output()
+        {
+            Console.WriteLine($"X = {x}\nY = {y}");
+        }
+    }
+}
+
---
+++ Задача 41/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point<int> point = new Point<int>(40, 50);
+            point.Output();
+            Console.ReadLine();
+
+        }
+    }
+}
