5d24e6291445a8053e53d60e527230e83b8ba3c1
Kab-35-16 <Kab-35-16@KAB-31-3->
2025-05-30 12:47:13 +0300

    Добавьте файлы проекта.

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum_45
+{
+    class Person <T>
+    {
+        public T Id { get; }
+        public string Name { get; }
+
+        public Person() //Конструктор без параметров
+        {
+            
+        }
+        public Person(T id, string name) //Конструктор с параметрами
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum_45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person<int> tom = new Person<int>(545, "Tom");
+            Company<Person<int>> microsoft = new Company<Person<int>>(tom);
+            Console.WriteLine(microsoft.CEO.Id);
+            Console.WriteLine(microsoft.CEO.Name);
+            /*Person<string> bob = new Person<string>("a1", "bob");
+            //Person tom = new Person(545, "Tom")
+            //Person bob = new Person("a1", "bob");
+            int tomId = (int)tom.Id;
+            string bobId = (string)bob.Id;
+
+            Console.WriteLine(tomId);
+            Console.WriteLine(bobId);*/
+            Console.ReadLine();
+        }
+    }
+}
