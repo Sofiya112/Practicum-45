0c60937a92e391d3bd734afe4a3c83f94d5bdca0
Sofiya112 <sofiya032005@gmail.com>
2025-05-30 12:50:39 +0300

    Add files via upload

---
+++ Задача 42/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle<int> triangleInt = new Triangle<int>(3, 4, 5);
+            triangleInt.PrintInfo();
+            Console.WriteLine($"Area: {triangleInt.CalculateArea()}");
+
+            // Пример использования для типа uint
+            Triangle<uint> triangleUInt = new Triangle<uint>(6, 8, 10);
+            triangleUInt.PrintInfo();
+            Console.WriteLine($"Area: {triangleUInt.CalculateArea()}");
+
+            // Пример использования для типа double
+            Triangle<double> triangleDouble = new Triangle<double>(7.5, 8.5, 9.5);
+            triangleDouble.PrintInfo();
+            Console.WriteLine($"Area: {triangleDouble.CalculateArea()}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задача 42/Triangle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_42
+{
+    class Triangle<T>
+    {
+        private T a { get; }
+        private T b { get; }
+        private T c { get; }
+
+        public Triangle(T a, T b, T c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($" a = {a}, b = {b}, c = {c}");
+        }
+        public double CalculateArea()
+        {
+            double sideA = Convert.ToDouble(a);
+            double sideB = Convert.ToDouble(b);
+            double sideC = Convert.ToDouble(c);
+
+            // Проверка на существование треугольника 
+            if (sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA)
+            {
+                // Полупериметр
+                double s = (sideA + sideB + sideC) / 2;
+                // Площадь по формуле Герона
+                return Math.Sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+            }
+            else
+            {
+                Console.WriteLine("Всё круто");
+                return 0;
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
